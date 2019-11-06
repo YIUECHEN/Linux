5e5fb5f6046f1087eb6f908e37d4442d271baab4
YIUECHEN <2268691528@qq.com>
2019-11-06 10:56:04 +0800

    change

--- pipe/pipe2.c
+++ pipe/pipe2.c
@@ -7,38 +7,44 @@
 int main()
 {
 int fd[2]={0};
+
+
 int ret=pipe(fd);
 if(ret<0){
 perror("pipe error");
 return -1;
 }
+
 int pid1=fork();
+
 if(pid1<0){
 perror("fork1 error");
 return -1;
-}else{
+}else if(pid1==0){ 
 //进程1实现ps
-close(0);
+close(fd[0]);
 dup2(fd[1],1);
 execlp("ps","ps","-ef",NULL);
-exit(0);;
+exit(0);
 }
 
 
 int pid2=fork();
+
 if(pid2<0){
 perror("fork2 error");
 return -1;
-}else{
+}else if(pid2==0){
 //进程2实现grep
+close(fd[1]);
 dup2(fd[0],0);
 execlp("grep","grep","ssh",NULL);
 exit(0);
 }
-close(0);
-close(1);
-waitpid(pid1,NULL,0);
-waitpid(pid2,NULL,0);
+close(fd[0]);
+close(fd[1]);
+//waitpid(pid1,NULL,0);
+//waitpid(pid2,NULL,0);
 return 0;
 }
 
