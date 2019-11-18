302ebe1cd21b01d75a87c10f5829518a41da9e2b
YIUECHEN <2268691528@qq.com>
2019-11-18 18:15:06 +0800

    practice

---
+++ systemV/shm_read.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<unistd.h>
+
+#define key 0x12345665
+#define size 4096
+
+int main()
+{       //创建共享内存
+	int shmid=shmget(key,size,IPC_CREAT|0664);
+
+	if(shmid<0){
+		perror("shmget error");
+		return -1;
+	}
+
+	//映射到虚拟地址
+	void *shm_start=shmat(shmid,NULL,0);
+
+	if(shm_start==(void*)-1){
+		perror("shmat error");
+		return -1;
+	}
+
+	int i=0;
+	while(1){
+		printf("%s\n",(char*)shm_start);
+		sleep(1);	
+	}
+	//解除映射关系
+	shmdt(shm_start);//shmat返回的指针
+	//控制共享内存	
+	shmctl(shmid,IPC_RMID,NULL);
+	return 0;
+
+}
